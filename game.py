f8f900024382b1fce92bdc3178fb2a945ae502bf
Arturas Aleksandraus <arty.aleksandraus@klarna.com>
2022-10-06 09:22:38 +0200

    improve astar

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import state, utils, click
 from heapq import heappop, heappush, heapify
 from functools import total_ordering
 import state_builder as sb
+import numba
 
 def player_input():
     print(
@@ -46,7 +47,7 @@ def display(game_state):
     print(utils.Sokoban)
     print(state.active_game_to_str(game_state))
 
-def unsavable(game):
+def unsavable(game, actions):
     level = game["active_level"]
     boxes = [k for k,v in level.items() if v == sb.box()]
     for bpos in boxes:
@@ -54,10 +55,23 @@ def unsavable(game):
             if state.is_at(game, next_pos(bpos, DELTA[f]), sb.wall())\
             and state.is_at(game, next_pos(bpos, DELTA[s]), sb.wall()):
                 return True
-            
+         
+        for w1, w2, b1 in ["aqw", "wed", "dcs", "sza", "azs", "scd", "dew", "wqa"]:
+            if state.is_at(game, next_pos(bpos, DELTA[w1]), sb.wall())\
+                and state.is_at(game, next_pos(bpos, DELTA[w2]), sb.wall())\
+                and state.is_at(game, next_pos(bpos, DELTA[b1]), sb.box()):
+                return True
+    done = len([item for item in state.items(game) if item == sb.combine(sb.storage(),sb.box()) ])
+    
+    # if done > 0:
+    #     print(done)
+    #     print(state.active_game_to_dstr(game))
+    # print((level["x"]+level["y"]), done, len(actions))
+    if (level["x"]+level["y"]) * 1.3 * (done+1) < len(actions):
+        return True   
+    # else:
+    
     return False
-
-        
 
 def score(game):
     level = game["active_level"]
@@ -71,12 +85,12 @@ def score(game):
             max_dist = max(max_dist, dist)
         tot_dist += max_dist
     return tot_dist
-
 @total_ordering
 class Wrapper:
     def __init__(self, game, game_state, actions):
         self.game_state, self.actions = game_state, actions
-        self.score = score(game)
+        self.score = score(game) + len(actions)
+        # self.score = len(actions)
     def __lt__(self, other):
         return self.score < other.score
  
@@ -86,7 +100,7 @@ class Wrapper:
     def items(self):
         return self.score, self.game_state, self.actions
 
-
+# @numba.jit(nopython=True)
 def solve(game_state):
     root = state.active_game_to_dstr(game_state)
     visited = set()
@@ -94,26 +108,37 @@ def solve(game_state):
     front = []
     heapify(front)
     heappush(front, Wrapper(game_state, root, []))
-
+    # front.append((root, []))
+    ms = 0
     while len(front) > 0:
         s, curr, actions = heappop(front).items()
-        print(s, len(visited))
+        # curr, actions = front.pop(0)
+        al = len(actions)
+        if ms < s:
+            ms = s
+            print(al, s-al, len(visited))
+            print(curr)
         # print(actions)
-        print(curr)
         curr_game = {"running": True, "active_level": state.parse_level(curr)}
         if done(curr_game):
+            print(actions)#lvl 1 231
+            print(len(visited))
             return actions
 
-        for action, dir in DELTA.items():
+        for action in "wasd":
             curr_game = {"running": True, "active_level": state.parse_level(curr)}
-            move(curr_game, dir)
+            move(curr_game, DELTA[action])
             next_state = state.active_game_to_dstr(curr_game)
             if next_state in visited:
                 continue
-            if unsavable(curr_game):
+            if unsavable(curr_game, actions):
+                # print(al, s-al, len(actions))
+                # print(next_state)
+                # exit()
                 continue
             visited.add(next_state)
             heappush(front, Wrapper(curr_game, next_state, actions + [action]))
+            # front.append((next_state, actions + [action]))
     return None
 
 
@@ -122,11 +147,16 @@ DELTA = {
     "a" : ( 0,-1),
     "s" : ( 1, 0),
     "d" : ( 0, 1),
+    "q" : ( -1, -1),
+    "e" : ( -1, 1),
+    "c" : ( 1, 1),
+    "z" : ( 1, -1),
 }
 
 def run(game_state):
     display(game_state)
     actions = []
+    moves = 0
     while not done(game_state):
         action = player_input()
         if action == "b":
@@ -141,6 +171,8 @@ def run(game_state):
         else:
             move(game_state, DELTA[action]) 
         display(game_state)
+        print("moves:", moves)
+        moves += 1
 
 if __name__ == "__main__":
     game_state = state.init()
